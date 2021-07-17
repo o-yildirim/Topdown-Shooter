85b9fe97027d5dfaab67a7522d37d5c0222032c6
Bontu380 <onyldrm@hotmail.com>
2021-07-17 13:38:19 +0300

    Keycard

---
+++ Topdown Shooter/Assets/Scripts/KeycardDoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeycardDoor : MonoBehaviour
+{
+    public bool isLocked = true;
+
+    public void unlock()
+    {
+        isLocked = false;
+    }
+
+    public void open()
+    {
+        //animator vs
+    }
+}
---
+++ Topdown Shooter/Assets/Scripts/PickableKeycard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableKeycard : PickableObject
+{
+    public KeycardDoor linkedDoor;
+    public GameObject infoCanvas;
+
+    public override void onPickup()
+    {
+        linkedDoor.unlock();
+        Destroy(gameObject);
+    }
+
+    public override void displayInfo()
+    {
+        infoCanvas.SetActive(true);
+    }
+
+    public override void hideInfo()
+    {
+        infoCanvas.SetActive(false);
+    }
+
+}
